1a534e9009dcff90699422c37a6c2b06112652cc
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-31 17:41:04 +0000

    Add Journey class and #initialize() and #arrive() methods and passing tests

---
+++ lib/journey.rb
@@ -0,0 +1,12 @@
+class Journey
+
+  attr_reader :entry_station, :exit_station
+
+  def initialize(entry_station)
+    @entry_station = entry_station
+  end
+
+  def arrive(exit_station)
+    @exit_station = exit_station
+  end
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,23 @@
+require 'journey'
+
+describe Journey do
+
+  let(:entry_station) { double("entry station") }
+  let(:exit_station) { double("exit station") }
+
+  subject(:journey) { described_class.new(entry_station) }
+
+  describe "#intialize" do
+    it "sets the entry station to the first argument" do
+      expect(journey.entry_station).to eq entry_station
+    end
+  end
+
+  describe "#arrive" do
+    it "sets the exit station to the passed argument" do
+      expect(journey.arrive(exit_station)).to eq exit_station
+    end
+  end
+
+
+end
